b6d7de796aa1e22dc9a9c865af8ff2e8c9594f9c
Kyle <127817535+kyle-j-bell@users.noreply.github.com>
2023-07-05 10:26:42 +0900

    Add files via upload

---
+++ person.js
@@ -0,0 +1,36 @@
+//Defines a Person class with firstName, lastName, and age instance variables.
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  //An instance method that prints a simple string "introducing" the Person object calling it.
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  //A static mmethod that checks if person is an Array and if each element is an instance of the Person class, then calls the introduce method on each element of person.
+  static introducePeople(person) {
+    if (!Array.isArray(person)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+    if (!person.every(person => person instanceof Person)) {
+      throw new Error("All items in array must be Person class instances.");
+    }
+    else {
+      person.forEach(person => person.introduce());
+    }
+  }
+}
+
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Person;
+} catch {
+  module.exports = null;
+}
